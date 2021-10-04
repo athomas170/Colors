8ce26e23257e1872067e5bc6b50cc4fec1e33356
athomas170 <91565775+athomas170@users.noreply.github.com>
2021-10-04 15:03:36 -0500

    Add files via upload

---
+++ Idle Coding.py
@@ -0,0 +1,15 @@
+#Initialize Turtle
+import turtle
+import random
+abby = turtle.Turtle()
+
+#Functions
+def randomDot():
+    abby.color("red")
+    abby.begin_fill()
+    abby.circle(100)
+    abby.end_fill()
+    
+
+#Main
+randomDot()
