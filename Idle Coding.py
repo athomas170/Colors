b53d25cba518ac10e4f73edeabe0cf52045d3a82
athomas170 <91565775+athomas170@users.noreply.github.com>
2021-10-06 14:37:34 -0500

    Update Idle Coding.py

--- Idle Coding.py
+++ Idle Coding.py
@@ -2,13 +2,28 @@
 import turtle
 import random
 abby = turtle.Turtle()
+turtle.colormode(255)
+abby.speed(500)
 
 #Functions
 def randomDot():
-    abby.color("red")
-    abby.begin_fill()
-    abby.circle(100)
-    abby.end_fill()
+    for i in range(200):
+        r=random.randint(0,255)
+        b=random.randint(0,255)
+        g=random.randint(0,255)
+        c=random.randint(0,200)
+        x=random.randint(-500,500)
+        y=random.randint(-500,500)
+        abby.penup()
+        abby.goto(x,y)
+        abby.pendown()
+        abby.color(r,b,g)
+        abby.begin_fill()
+        abby.circle(c)
+        abby.end_fill()
+        print(r)
+        print(b)
+        print(g)
     
 
 #Main
